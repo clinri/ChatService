26c287ebb8c8141bfdd894092153ad1ed366e473
Andrei Borisov <clinri@ya.ru>
2022-04-18 23:37:55 +0300

    do sendMessage

--- src/main/kotlin/ru/netology/data/Message.kt
+++ src/main/kotlin/ru/netology/data/Message.kt
@@ -1,6 +1,8 @@
 package ru.netology.data
 
 data class Message(
-    val idMessage: Int,
-    val idChat: Int
+    val idMessage: Int = 0,
+    val ownerId: Int,
+    val message: String,
+    val isRead: Boolean = false
 )
--- src/main/kotlin/ru/netology/service/ChatsService.kt
+++ src/main/kotlin/ru/netology/service/ChatsService.kt
@@ -1,11 +1,15 @@
 package ru.netology.service
 
 import ru.netology.data.Chat
+import ru.netology.data.Message
 
 object ChatsService {
     private val chats = mutableListOf<Chat>()
+    private var chatsCount = 1
+    private var messageCount = 1
 
     /**     Chat (CRUD)     */
+    /**create*/
     fun createChat(idUser: Int) {
 
     }
@@ -25,20 +29,42 @@ object ChatsService {
     }
 
     /**     Message (CRUD)     */
+    /** create */
+    fun sendMessage(
+        userSenderId: Int,
+        userReceiverId: Int,
+        message: String
+    ) {
+        val newMessageId = messageCount++
+        val messageToChat = Message(newMessageId, userSenderId, message)
+        val chat = chats.filter { chat ->
+            chat.users.containsAll(listOf(userSenderId, userReceiverId))
+        }
+            .firstOrNull()
+            ?.let { chat ->
+                chat.copy(messages = chat.messages + messageToChat)
+            } ?: {
+            val newChatId = chatsCount++
+            Chat(
+                newChatId,
+                listOf(userSenderId, userReceiverId),
+                listOf(messageToChat)
+            )
+        }
+    }
+
+
     /**
      * Cписок сообщений
      * после того, как вызвана данная функция, все отданные сообщения автоматически считаются прочитанными
      */
     fun getMessages(
-        idUser: Int,
+
         idChat: Int,
         idOffset: Int, //id последнего сообщения, начиная с которого нужно подгрузить более новые
         count: Int
     ) {
 
-    }
-
-    fun createMessage(idUser: Int) {
 
     }
 
